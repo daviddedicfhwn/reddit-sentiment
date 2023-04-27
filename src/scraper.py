f44466f25af8d3143a981dfad14ab19b25a067d9
Noah <thenoahleuenberger@gmail.com>
2023-04-27 21:25:45 +0200

    Removed unused variables

--- src/scraper.py
+++ src/scraper.py
@@ -15,7 +15,6 @@ class SubredditScraper:
     def __init__(self, driver_options):
         self.driver_options = driver_options
 
-
     def scrape_subreddit(self, subreddit_id):
         with get_driver(self.driver_options) as driver:
             driver.get(self.get_subreddit_url(subreddit_id))
@@ -33,12 +32,7 @@ class SubredditScraper:
         post_elements = driver.find_elements(By.XPATH, "//div[@data-testid='post-container']//a[@data-click-id='body']")
         post_hrefs = [element.get_attribute('href') for element in post_elements]
 
-        df_posts = pl.DataFrame(
-            schema=[('post_id', pl.Utf8), ('author', pl.Utf8), ('subreddit', pl.Utf8), ('title', pl.Utf8),
-                    ('permalink', pl.Utf8)])
-
         count = 1
-
         for href in post_hrefs:
             try:
                 driver.get(href)
