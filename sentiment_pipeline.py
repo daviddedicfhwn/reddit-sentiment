db2e0694c00dbaed31540e6e06b64c2ffb78dc63
Nils <fahrni.nils@gmail.com>
2023-05-01 16:09:57 +0200

    :construction: added vscode conf folder to gitignore

--- sentiment_pipeline.py
+++ sentiment_pipeline.py
@@ -1,10 +1,17 @@
 from transformers import pipeline
+import asyncio
 
 class SentimentPipeline:
     def __init__(self, pipeline_name="sentiment-analysis"):
         self.pipeline = pipeline(pipeline_name)
 
     def get_sentiments(self, data):
+        if len(data) > 100):
+            return self.get_sentiment_batches(data)
         sentiment =  self.pipeline(data)
         return sentiment, data
-
+    
+    def get_sentiment_batches(self, data):
+        sentiments = []
+        
+    
