0172df13f6f5c624902d0179ce679af7b1436815
Noah <thenoahleuenberger@gmail.com>
2023-05-15 13:59:34 +0200

    Fixed unit tests to match modified scraper and added docs

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,45 +1,79 @@
 import unittest
 from unittest.mock import MagicMock, patch
 
-import polars as pl
-from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.wait import WebDriverWait
+import polars
+from selenium.common import WebDriverException
 
+from src import config
 from src.scraper import SubredditScraper
 
 
 class TestSubredditScraper(unittest.TestCase):
+    """
+    Unit Test class for the SubredditScraper class.
 
-    @patch('src.scraper.get_driver')
-    def test_scrape_subreddit(self, mock_get_driver):
-        mock_driver = MagicMock(spec=WebDriver)
-        mock_driver.current_url = 'https://www.reddit.com/r/test_subreddit/'
+    Attributes:
+        driver_options: A MagicMock object representing options for the webdriver.
+        db_client: A MagicMock object representing a database client.
+        subreddit_scraper: A SubredditScraper object to perform unit tests on.
 
-        mock_get_driver.return_value.__enter__.return_value = mock_driver
+    Methods:
+        setUpClass: Set up test class with common test objects.
+        test_scrape_subreddit: Test the scrape_subreddit method of the SubredditScraper class.
+        test_extract_post_data: Test the extract_post_data method of the SubredditScraper class.
+        test_process_post: Test the process_post method of the SubredditScraper class.
+        test_process_post_with_exception: Test the process_post method of the SubredditScraper class when an exception is raised.
+        test_extract_author: Test the extract_author method of the SubredditScraper class.
+        test_get_subreddit_url: Test the get_subreddit_url method of the SubredditScraper class.
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.driver_options = MagicMock()
+        cls.db_client = MagicMock()
+        cls.subreddit_scraper = SubredditScraper(cls.driver_options, cls.db_client)
 
-        scraper = SubredditScraper(None, None)
-        scraper.extract_post_data = MagicMock()
+    @patch("src.scraper.get_driver")
+    @patch("src.scraper.handle_cookie_banner")
+    @patch("src.scraper.scroll_to_bottom")
+    @patch("src.scraper.WebDriverWait")
+    def test_scrape_subreddit(self, mock_wait, mock_scroll, mock_handle_cookie, mock_get_driver):
+        subreddit_id = 'test_subreddit'
+        max_posts = 10
 
-        scraper.scrape_subreddit("test_subreddit", None)
+        driver_mock = MagicMock()
+        mock_get_driver.return_value.__enter__.return_value = driver_mock
+        self.subreddit_scraper.extract_post_data = MagicMock()
 
-        mock_get_driver.assert_called_once()
-        scraper.extract_post_data.assert_called_once_with(mock_driver, None)
+        self.subreddit_scraper.scrape_subreddit(subreddit_id, max_posts)
 
-    @patch('src.database.MongoDBClient.insert_many_data')
-    @patch('src.scraper.SubredditScraper.extract_comments_data')
-    def test_extract_post_data(self, mock_extract_comments_data, mock_insert_many_data):
-        mock_driver = MagicMock(spec=WebDriver)
+        mock_get_driver.assert_called_once_with(self.driver_options)
+        driver_mock.get.assert_called_once_with(self.subreddit_scraper.get_subreddit_url(subreddit_id))
+        mock_handle_cookie.assert_called_once_with(driver_mock)
+        mock_scroll.assert_called_once_with(driver_mock, config.SCROLL_TIME)
+        self.subreddit_scraper.extract_post_data.assert_called_once_with(driver_mock, subreddit_id, max_posts)
 
-        # Create the WebElement mock and set the get_attribute return value
-        mock_element = MagicMock(spec=WebElement)
-        mock_element.get_attribute.return_value = 'https://www.reddit.com/r/test_subreddit/comments/p1/post_title/'
+    @patch("src.scraper.WebDriverWait")
+    def test_extract_post_data(self, mock_wait):
+        driver_mock = MagicMock()
+        subreddit_id = 'test_subreddit'
+        max_posts = 10
+        post_elements = [MagicMock(), MagicMock()]
+        driver_mock.find_elements.return_value = post_elements
+        self.subreddit_scraper.process_post = MagicMock()
 
-        mock_driver.find_elements.return_value = [mock_element]
+        self.subreddit_scraper.extract_post_data(driver_mock, subreddit_id, max_posts)
 
-        scraper = SubredditScraper(None, None)
+        driver_mock.find_elements.assert_called_once()
+        self.subreddit_scraper.process_post.assert_called()
 
-        mock_extract_comments_data.return_value = pl.DataFrame(
+    @patch('src.scraper.config')
+    @patch('src.scraper.pl')
+    @patch('src.scraper.WebDriverWait')
+    def test_process_post(self, mock_wait, mock_pl, mock_config):
+        mock_scraper = MagicMock()
+        mock_scraper.extract_author.return_value = "author"
+
+        test_df = polars.DataFrame(
             {
                 'post_id': ['p1'],
                 'text': ['comment text'],
@@ -51,34 +85,52 @@ class TestSubredditScraper(unittest.TestCase):
             }
         )
 
-        with patch.object(WebDriverWait, 'until', return_value=True):
-            scraper.extract_post_data(mock_driver)
+        mock_scraper.extract_comments_data.return_value = test_df
 
-        self.assertEqual(mock_insert_many_data.call_count, 0)
-        mock_extract_comments_data.assert_called_once_with(mock_driver, 'p1')
+        driver_mock = MagicMock()
 
-    def test_extract_comments_data(self):
-        mock_driver = MagicMock(spec=WebDriver)
-        mock_comment_element = MagicMock(spec=WebElement)
-        mock_driver.find_elements.return_value = [mock_comment_element]
+        mock_db_client = MagicMock()
+        mock_db_client.__enter__.return_value = mock_db_client
+        mock_scraper.db_client = mock_db_client
 
-        scraper = SubredditScraper(None, None)
+        mock_logger = MagicMock()
+        mock_scraper.logger = mock_logger
 
-        # Mock the necessary WebElement attributes and methods
-        mock_comment_element.get_attribute.side_effect = lambda attr: {
-            "author": "test_author",
-            "thingid": "t1",
-            "parentid": "p1",
-            "score": "10",
-            "permalink": "/r/test_subreddit/comments/.../"
-        }.get(attr, "")
-        mock_comment_element.find_element.return_value.text = "comment text"
+        href = 'https://www.reddit.com/r/test_subreddit/comments/post_id/title'
 
-        df_comments = scraper.extract_comments_data(mock_driver, "p1")
+        SubredditScraper.process_post(mock_scraper, driver_mock, href)
 
-        self.assertEqual(len(df_comments), 1)
-        self.assertEqual(df_comments['post_id'][0], 'p1')
+        driver_mock.get.assert_called_once_with(href)
+        mock_scraper.extract_author.assert_called_once_with(driver_mock)
+        mock_scraper.extract_comments_data.assert_called_once_with(driver_mock, 'post_id')
+        mock_db_client.insert_many_data.assert_any_call(mock_config.COMMENTS_COLLECTION, test_df.to_dicts())
+
+    def test_process_post_with_exception(self):
+        mock_scraper = MagicMock()
+        driver_mock = MagicMock()
+        driver_mock.get.side_effect = WebDriverException
+
+        href = 'https://www.reddit.com/r/test_subreddit/comments/post_id/title'
+
+        SubredditScraper.process_post(mock_scraper, driver_mock, href)
+
+        driver_mock.get.assert_called_once_with(href)
+        driver_mock.refresh.assert_called_once()
+
+    @patch("src.scraper.WebDriverWait")
+    def test_extract_author(self, mock_wait):
+        driver_mock = MagicMock()
+        author_element = MagicMock()
+        author_element.text = 'test_author'
+        driver_mock.find_element.return_value = author_element
+        self.assertEqual(self.subreddit_scraper.extract_author(driver_mock), 'test_author')
+        driver_mock.find_element.assert_called_once()
+
+    def test_get_subreddit_url(self):
+        subreddit_id = 'test_subreddit'
+        expected_url = 'https://www.reddit.com/r/test_subreddit'
+        self.assertEqual(self.subreddit_scraper.get_subreddit_url(subreddit_id), expected_url)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
--- tests/test_site.py
+++ tests/test_site.py
@@ -8,7 +8,18 @@ from tests.test_constants import TEST_SUBREDDIT_ID, TEST_SCROLL_TIME
 
 
 class TestSiteElements(unittest.TestCase):
+    """
+    Unit Test class for Site Elements.
 
+    Methods:
+        setUp: Set up test environment by creating a Selenium webdriver and navigating to a test subreddit.
+        tearDown: Close the Selenium webdriver after the test.
+        test_posts_container_presence: Test for the presence of a posts container on the subreddit page.
+        test_post_body_link_presence: Test for the presence of a post body link in the posts container.
+        test_comment_presence: Test for the presence of comments on a post page.
+        test_author_name_presence: Test for the presence of the author's name on a post page.
+        test_cookie_banner_presence: Test for the presence of a cookie banner on the subreddit page.
+    """
     def setUp(self):
         self.driver = get_driver(driver_options=Options())
         self.driver.get(f'https://www.reddit.com/r/{TEST_SUBREDDIT_ID}')
@@ -59,21 +70,6 @@ class TestSiteElements(unittest.TestCase):
                     EC.presence_of_element_located((By.XPATH, "//a[contains(@class, 'author-name')]")))
             else:
                 self.fail("No post was found to check for author name")
-        except Exception as e:
-            self.fail(f"Test failed due to exception: {str(e)}")
-
-    def test_upvotes_presence(self):
-        try:
-            scroll_to_bottom(self.driver, TEST_SCROLL_TIME)
-            post_elements = self.driver.find_elements(By.XPATH,
-                                                      "//div[@data-testid='post-container']//a[@data-click-id='body']")
-            if post_elements:
-                post_href = post_elements[0].get_attribute('href')
-                self.driver.get(post_href)
-                WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, "//div[contains(@class, '_3-6v7')]")))
-            else:
-                self.fail("No post was found to check for upvotes")
         except Exception as e:
             self.fail(f"Test failed due to exception: {str(e)}")
 
